315e8413b2f6eea367b6e7c8331afcf4598b9c99
Ryusuke Tadagawa <tadagawa@torem.co.jp>
2024-03-18 12:57:16 +0900

    プログラムの追加 READMEの修正

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NF48VsCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
